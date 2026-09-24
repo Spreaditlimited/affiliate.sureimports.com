a8a76dad552ccc764cd068410915d8abefd8f691
Tochukwu Nkwocha <nkwochatochukwu@gmail.com>
2026-09-24 13:02:24 +0100

    Serve footer logo locally within image security policy

--- app/(home)/components/SiteFooter.tsx
+++ app/(home)/components/SiteFooter.tsx
@@ -2,5 +2,5 @@ import BrandFooter from '@/components/BrandFooter';
 import FooterNewsletterForm from '@/components/FooterNewsletterForm';
 
 export function SiteFooter() {
-  return <BrandFooter home="https://www.sureimports.com" mainSite="https://www.sureimports.com" logo="https://www.sureimports.com/images/svg-logo-white.svg" newsletter={<FooterNewsletterForm />} />;
+  return <BrandFooter home="https://www.sureimports.com" mainSite="https://www.sureimports.com" logo="/images/svg-logo-white.svg" newsletter={<FooterNewsletterForm />} />;
 }
